82a98508a7c689812e54246cc638e3154db5a9c1
Lokesh Chamane <lokesh.c703@gmail.com>
2017-12-21 17:23:50 -0500

    taimen: Don't look for octos.dependencies
    Change-Id: I6b6a5dbae30a771a3d4175cdfcfef84b280b64af
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- octos.mk
+++ octos.mk
@@ -4,9 +4,6 @@ TARGET_SCREEN_WIDTH := 1440
 
 $(call inherit-product, vendor/octos/config/common_full_phone.mk)
 
-# Inherit OctOS configuration
-$(call inherit-product, vendor/octos/dependencies/taimen.dependencies)
-
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
@@ -22,4 +19,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
     BUILD_FINGERPRINT=google/taimen/taimen:8.0.0/OPD1.170816.025/4424668:user/release-keys \
     PRIVATE_BUILD_DESC="taimen-user 8.0.0 OPD1.170816.025 4424668 release-keys"
-
